9546c81d592d9bbe2077be64e20ce41677d93473
MaxCode-AC <kemax3903@gmail.com>
2025-03-10 03:38:15 +0530

    feat: Add Car API with CRUD operations

---
+++ src/main/java/com/megacity/cab/repository/CarRepository.java
@@ -0,0 +1,7 @@
+package com.megacity.cab.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.megacity.cab.model.Car;
+
+public interface CarRepository extends JpaRepository<Car, Long> {
+}
